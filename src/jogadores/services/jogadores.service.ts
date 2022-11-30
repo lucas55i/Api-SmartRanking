4c56bdb98a0da7cb9ede5f02e39370a3b076c018
lucas55i <lucas_55i@Outlook.com>
2022-11-29 21:48:35 -0300

    Criação do método de deletar jogador pelo e-mail (Service)

--- src/jogadores/services/jogadores.service.ts
+++ src/jogadores/services/jogadores.service.ts
@@ -37,6 +37,15 @@ export class JogadoresService {
     return jogadorEncontrado;
   }
 
+  async deletarJogador(email: string): Promise<void> {
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+    this.jogadores = this.jogadores.filter(
+      (jogador) => jogador.email !== jogadorEncontrado.email,
+    );
+  }
+
   private criar(criarJogadorDto: CriarJogadorDto): void {
     const { nome, telefoneCelular, email } = criarJogadorDto;
 
