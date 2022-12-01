51fadf87d1a8266861ccbe36d68c9667f907e173
lucas55i <lucas_55i@Outlook.com>
2022-11-30 21:08:19 -0300

    Registrando o schema de jogadores ao escopo do modulo de jogadores

--- src/jogadores/jogadores.module.ts
+++ src/jogadores/jogadores.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresController } from './controller/jogadores.controller';
+import { JogadorSchema } from './schemas/jogador.schema';
 import { JogadoresService } from './services/jogadores.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Jogador', schema: JogadorSchema }]),
+  ],
   controllers: [JogadoresController],
   providers: [JogadoresService],
 })
