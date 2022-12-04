581e9614ee8fe5b8dfb60ede3ae30aa13a9a4139
lucas55i <lucas_55i@Outlook.com>
2022-12-04 14:26:11 -0300

    Criação do modulo de categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
       'mongodb+srv://m001-studente:m001-mongodb-basics@cluster0.ty01u8u.mongodb.net/smartranking?retryWrites=true&w=majority',
     ),
     JogadoresModule,
+    CategoriasModule,
   ],
   controllers: [],
   providers: [],
