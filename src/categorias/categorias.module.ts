98f778f69cea8cb28d3cae256687354e26fa39fe
lucas55i <lucas_55i@Outlook.com>
2022-12-14 09:18:20 -0300

    Export do modulos de jogadores

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Mongoose } from 'mongoose';
+import { JogadoresModule } from 'src/jogadores/jogadores.module';
 import { CategoriasController } from './controller/categorias.controller';
 import { CategoriaSchema } from './schema/categoria.schema';
 import { CategoriasService } from './services/categorias.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }])],
+  imports: [MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }]), JogadoresModule],
   controllers: [CategoriasController],
   providers: [CategoriasService]
 })
