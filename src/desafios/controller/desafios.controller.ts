f8a7a9492a450110297dd14feb786e50fc6efd2d
lucas55i <lucas_55i@Outlook.com>
2022-12-14 20:58:56 -0300

    Criação do controller de desafio

---
+++ src/desafios/controller/desafios.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CriarDesafioDto } from '../dtos/criar-desafio.dto';
+import { Desafio } from '../interfaces/desafio.interface';
+import { DesafiosService } from '../service/desafios.service';
+
+@Controller('api/v1/desafios')
+export class DesafiosController {
+
+    constructor(private readonly desafiosService: DesafiosService){}
+
+    private readonly logger =  new Logger(DesafiosController.name)
+
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async criaDesafio(@Body() criarDesafio: CriarDesafioDto): Promise<Desafio>{
+        this.logger.log(`criarDesafioDto: ${JSON.stringify(criarDesafio)}`)
+        return await this.desafiosService.criarDesafio(criarDesafio)
+
+    }
+}
