08dfd4d821ec7dca476553f37bdbd4591301b44e
lucas55i <lucas_55i@Outlook.com>
2022-12-14 21:00:24 -0300

    Criação do DTO de criar desafio

---
+++ src/desafios/dtos/criar-desafio.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsDateString, IsArray, ArrayMinSize, ArrayMaxSize } from "class-validator";
+import { Jogador } from "src/jogadores/interfaces/jogador.interface";
+
+export class CriarDesafioDto{
+
+    @IsNotEmpty()
+    @IsDateString()
+    dataHoraDesafio: Date;
+  
+    @IsNotEmpty()
+    solicitante: Jogador;
+  
+    @IsArray()
+    @ArrayMinSize(2)
+    @ArrayMaxSize(2)
+    jogadores: Array<Jogador>
+  
+}
