2af4fef1bcddc0c636fda476a6fdbe043ffc9d65
lucas55i <lucas_55i@Outlook.com>
2022-12-16 12:22:36 -0300

    Implementação dos metétodos de consultar todos os desafios e desafios de um jogador

--- src/desafios/controller/desafios.controller.ts
+++ src/desafios/controller/desafios.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CriarDesafioDto } from '../dtos/criar-desafio.dto';
 import { Desafio } from '../interfaces/desafio.interface';
 import { DesafiosService } from '../service/desafios.service';
@@ -6,16 +6,20 @@ import { DesafiosService } from '../service/desafios.service';
 @Controller('api/v1/desafios')
 export class DesafiosController {
 
-    constructor(private readonly desafiosService: DesafiosService){}
+    constructor(private readonly desafiosService: DesafiosService) { }
 
-    private readonly logger =  new Logger(DesafiosController.name)
+    private readonly logger = new Logger(DesafiosController.name)
 
 
     @Post()
     @UsePipes(ValidationPipe)
-    async criaDesafio(@Body() criarDesafioDto: CriarDesafioDto): Promise<Desafio>{
+    async criaDesafio(@Body() criarDesafioDto: CriarDesafioDto): Promise<Desafio> {
         this.logger.log(`criarDesafioDto: ${JSON.stringify(criarDesafioDto)}`)
         return await this.desafiosService.criarDesafio(criarDesafioDto)
+    }
 
+    @Get()
+    async consultarDesafios(@Query('idJogador') _id: string): Promise<Array<Desafio>> {
+        return _id ? await this.desafiosService.consultarDesafiosDeUmJogador(_id) : await this.desafiosService.consultarTodosDesafios()
     }
 }
