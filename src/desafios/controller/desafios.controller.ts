32e5edee447f0a263ad5a284d04a98fddb3a502c
lucas55i <lucas_55i@Outlook.com>
2022-12-16 12:36:33 -0300

    Implemtatção do metodo de atualizarDesafio (CONTROLLER)

--- src/desafios/controller/desafios.controller.ts
+++ src/desafios/controller/desafios.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Get, Logger, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AtualizarCategoriaDto } from 'src/categorias/dtos/atualizar-categoria.dto';
+import { AtualizarDesafioDto } from '../dtos/atualizar-desafio.dto';
 import { CriarDesafioDto } from '../dtos/criar-desafio.dto';
 import { Desafio } from '../interfaces/desafio.interface';
+import { DesafioStatusValidacaoPipe } from '../pipes/desfio-status-validation.pipe';
 import { DesafiosService } from '../service/desafios.service';
 
 @Controller('api/v1/desafios')
@@ -21,5 +24,10 @@ export class DesafiosController {
     @Get()
     async consultarDesafios(@Query('idJogador') _id: string): Promise<Array<Desafio>> {
         return _id ? await this.desafiosService.consultarDesafiosDeUmJogador(_id) : await this.desafiosService.consultarTodosDesafios()
+    }
+
+    @Put('/:desafio')
+    async atualizarDesafio(@Body(DesafioStatusValidacaoPipe) atualizarDesafioDto: AtualizarDesafioDto, @Param('desafio') _id: string): Promise<void> {
+        await this.desafiosService.atualizarDesafio(_id, atualizarDesafioDto)
     }
 }
